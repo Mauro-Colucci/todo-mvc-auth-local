5cb2b692391691921db5d545ef75952ab07133ae
Mauro Colucci <colucci.mauro@gmail.com>
2022-09-03 15:24:35 -0300

    MVP. crud functionality achieved

--- controllers/hitlist.js
+++ controllers/hitlist.js
@@ -5,11 +5,15 @@ module.exports = {
         console.log(req.user)
         try{
             const entryItems = await Hitlist.find({userId:req.user.id})
-            //res.render('todos.ejs', {entry: entryItems, user: req.user})
-            res.status(200).json(entryItems)
+            //const entryTotal = await Hitlist.countDocuments()
+            res.render('hitlist.ejs', {entries: entryItems, numEntries: entryItems.length,  user: req.user})
+            //res.status(200).json(entryItems)
         }catch(err){
             console.log(err)
         }
+    },
+    getEntry: (req, res)=>{
+        res.render('form.ejs')
     },
     postEntry: async (req, res)=>{
         try{
@@ -22,17 +26,27 @@ module.exports = {
                 source: req.body.source,
                 userId: req.user.id
             })
-            res.status(200).json(entry)
-            //res.redirect('/todos')
+            //res.status(200).json(entry)
+            res.redirect('/hitlist')
         }catch(err){
             console.log(err)
+        }
+    },
+    getEntryToEdit: async (req, res)=>{
+        try {
+            const singleEntry = await Hitlist.findById(req.params.id)
+            res.render('editForm', {entry: singleEntry})
+            //res.status(200).json(singleEntry)
+        } catch (err) {
+            console.error(err)
         }
     },
     putEntry: async (req, res)=>{
         try{
             //await Hitlist.findOneAndUpdate({_id:req.body.todoIdFromJSFile},req.body)
             const updatedEntry = await Hitlist.findByIdAndUpdate(req.params.id, req.body,{new: true})
-            res.status(200).json(updatedEntry)
+            //res.status(200).json(updatedEntry)
+            res.redirect('/hitlist')
         }catch(err){
             console.log(err)
         }
@@ -42,7 +56,8 @@ module.exports = {
         try{
             //await Hitlist.findOneAndDelete({_id:req.body.todoIdFromJSFile})
             await Hitlist.findByIdAndDelete(req.params.id)
-            res.json('Deleted')
+            //res.json('Deleted')
+            res.redirect('/hitlist')
         }catch(err){
             console.log(err)
         }
--- routes/hitlist.js
+++ routes/hitlist.js
@@ -5,8 +5,10 @@ const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
 router.get('/', ensureAuth, hitlistController.getEntries)
 
+router.get('/addEntry', hitlistController.getEntry)
 router.post('/addEntry', hitlistController.postEntry)
 
+router.get('/updateEntry/:id', hitlistController.getEntryToEdit)
 router.put('/updateEntry/:id', hitlistController.putEntry)
 
 router.delete('/deleteEntry/:id', hitlistController.deleteEntry)
